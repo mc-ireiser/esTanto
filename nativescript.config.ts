e4bec99712fcbb891d7a31c7a9aecb75c52d60ed
Jesus Rodriguez <mc.ireiser@gmail.com>
2021-10-09 18:35:01 -0400

    Lint

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,11 +1,11 @@
-import { NativeScriptConfig } from '@nativescript/core';
+import { NativeScriptConfig } from "@nativescript/core";
 
 export default {
-  id: 'org.nativescript.alcambio',
-  appPath: 'app',
-  appResourcesPath: 'App_Resources',
+  id: "me.ireiser.alcambio",
+  appPath: "app",
+  appResourcesPath: "App_Resources",
   android: {
-    v8Flags: '--expose_gc',
-    markingMode: 'none'
+    v8Flags: "--expose_gc",
+    markingMode: "none"
   }
 } as NativeScriptConfig;
