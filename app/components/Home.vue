a1523091557cbaeedecd8a5f01967bf5fc760a5b
Jesus Rodriguez <mc.ireiser@gmail.com>
2021-10-08 20:48:08 -0400

    Base user input

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,43 +1,51 @@
 <template>
   <Page>
     <ActionBar>
-      <Label text="Home" />
+      <Label text="alCambio" />
     </ActionBar>
-
-    <GridLayout>
-      <Label class="info">
-        <FormattedString>
-          <Span class="fas" text.decode="&#xf135; " />
-          <Span :text="message" />
-        </FormattedString>
-      </Label>
-    </GridLayout>
+    <StackLayout>
+      <StackLayout width="320" margin="20" padding="20">
+        <Label
+          text="Conversor Monetario"
+          textWrap="true"
+          fontSize="16"
+          fontWeight="bold"
+          textAlignment="center"
+        />
+        <GridLayout rows="auto" columns="*,*,*">
+          <StackLayout col="0" horizontalAlignment="center">
+            <Label text="De" textWrap="true" />
+            <Label text="USD" textWrap="true" />
+          </StackLayout>
+          <StackLayout col="1" horizontalAlignment="center">
+            <Label>
+              <FormattedString>
+                <Span class="fas" text.decode="&#xf362; " />
+              </FormattedString>
+            </Label>
+          </StackLayout>
+          <StackLayout col="2" horizontalAlignment="center">
+            <Label text="A" textWrap="true" />
+            <Label text="VEF" textWrap="true" />
+          </StackLayout>
+        </GridLayout>
+        <Label
+          text="Monto en USD"
+          textWrap="true"
+          fontSize="14"
+          textAlignment="center"
+        />
+        <TextField keyboardType="number" />
+      </StackLayout>
+    </StackLayout>
   </Page>
 </template>
 
 <script lang="ts">
 import Vue from "nativescript-vue";
 
-export default Vue.extend({
-  computed: {
-    message() {
-      return "Blank {N}-Vue app";
-    },
-  },
-});
+export default Vue.extend({});
 </script>
 
 <style scoped lang="scss">
-@import "@nativescript/theme/scss/variables/blue";
-
-// Custom styles
-.fas {
-  @include colorize($color: accent);
-}
-
-.info {
-  font-size: 20;
-  horizontal-align: center;
-  vertical-align: center;
-}
 </style>
