ff8a26ba6941fbe79583d73d00d2586b908c74e4
Jesus Rodriguez <mc.ireiser@gmail.com>
2021-10-08 22:46:49 -0400

    Add list of values

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,14 +1,20 @@
 <template>
-  <Page>
-    <ActionBar>
-      <Label text="alCambio" />
+  <Page actionBarHidden="true">
+    <ActionBar flat="true">
+      <Label text="alCambio" fontSize="24" />
     </ActionBar>
-    <StackLayout>
-      <StackLayout width="320" margin="20" padding="20">
+    <GridLayout
+      rows="auto, auto, *"
+      columns="*"
+      width="320"
+      margin="25"
+      padding="20"
+    >
+      <StackLayout row="0" marginBottom="20">
         <Label
-          text="Conversor Monetario"
+          text="alCambio"
           textWrap="true"
-          fontSize="20"
+          fontSize="24"
           marginBottom="25"
           fontWeight="bold"
           textAlignment="center"
@@ -52,21 +58,69 @@
           text="Monto en USD"
           textWrap="true"
           fontSize="16"
+          marginBottom="15"
           textAlignment="center"
         />
         <TextField keyboardType="number" />
       </StackLayout>
-    </StackLayout>
+      <StackLayout row="1">
+        <Label
+          text="Octubre 8, 2021 03:21 PM"
+          fontSize="14"
+          textWrap="true"
+          marginBottom="15"
+          horizontalAlignment="center"
+        />
+        <label class="line"></label>
+      </StackLayout>
+      <ListView row="2" for="item in listOfValues" marginTop="15">
+        <v-template>
+          <StackLayout>
+            <Label :text="item.ref" textWrap="true" />
+            <Label>
+              <FormattedString>
+                <Span :text="item.value" fontSize="24" />
+                <Span text=" VEF" />
+              </FormattedString>
+            </Label>
+          </StackLayout>
+        </v-template>
+      </ListView>
+    </GridLayout>
   </Page>
 </template>
 
 <script lang="ts">
 import Vue from "nativescript-vue";
 
-export default Vue.extend({});
+export default Vue.extend({
+  data() {
+    return {
+      listOfValues: [
+        {
+          value: 154.7,
+          ref: "x",
+        },
+        {
+          value: 156.15,
+          ref: "y",
+        },
+        {
+          value: 156.15,
+          ref: "z",
+        },
+      ],
+    };
+  },
+});
 </script>
 
 <style scoped lang="scss">
+.line {
+  background: linear-gradient(to left, #28bfba, green, lightblue);
+  height: 2px;
+}
+
 .currency-tag {
   width: 50;
   padding: 6;
