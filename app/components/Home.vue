30b07f779343fe9b7b10eae60454c14dcf675b1c
Jesus Rodriguez <mc.ireiser@gmail.com>
2021-10-09 23:35:48 -0400

    Add Disclaimer

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -158,6 +158,17 @@ export default Vue.extend({
   },
 
   created() {
+    if (!ApplicationSettings.getBoolean("tos")) {
+      alert({
+        title: "Descargo de responsabilidad",
+        message:
+          "Toda la información en la aplicación se proporciona 'tal cual', sin garantía de integridad, exactitud, puntualidad o de los resultados obtenidos del uso de esta información, y sin garantía de ningún tipo, expresa o implícita. Las tasas referenciales mostradas son públicas y externas a la aplicación, no se guarda relación de ningún tipo con las mismas. Las referencias hechas a nombres de terceros, logotipos y marcas registradas se incluyen estrictamente para uso referencial y son propiedad de sus respectivos propietarios.",
+        okButtonText: "Acepto",
+      }).then(() => {
+        ApplicationSettings.setBoolean("tos", true);
+      });
+    }
+
     this.asyncData();
   },
 
