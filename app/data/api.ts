62aa05867d61ad55ae5e808cdb89fa78c3ef9046
Jesus Rodriguez <mc.ireiser@gmail.com>
2021-10-11 17:14:15 -0400

    Add data files

---
+++ app/data/api.ts
@@ -0,0 +1,3 @@
+const API_URL = "https://s3.amazonaws.com/dolartoday/data.json";
+
+export default API_URL;
---
+++ app/data/disclaimerDialog.ts
@@ -0,0 +1,5 @@
+export default {
+  title: "Descargo de responsabilidad",
+  message:
+    "Toda la información en la aplicación se proporciona 'tal cual', sin garantía de integridad, exactitud, puntualidad o de los resultados obtenidos del uso de esta información, y sin garantía de ningún tipo, expresa o implícita. Las tasas referenciales mostradas son públicas y externas a la aplicación, no se guarda relación de ningún tipo con las mismas. Las referencias hechas a nombres de terceros, logotipos y marcas registradas se incluyen estrictamente para uso referencial y son propiedad de sus respectivos propietarios."
+};
